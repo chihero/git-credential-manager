077829a9ee6e27bc19dbc4eda49f27287e4fc1c5
Matthew John Cheetham <mjcheetham@outlook.com>
2022-10-12 15:58:50 -0700

    wsl: detect host Windows session 0 and disable browser
    In order to detect if we have an interactive Windows desktop session
    from inside WSL we need to 'punch out' from WSL to determine the session
    ID and window station.
    
    Strictly speaking, except for session 0 (from Vista onwards), any
    Windows session can have exactly one interactive window station (always
    called WinSta0). However, because we cannot easily determine the window
    station name from a simple cmd/powershell script, we take a simplified
    approach which isn't 100% accurate.
    
    Instead, we only permit browser auth methods if we are NOT in Windows
    session 0; any other Windows session we assume we are in WinSta0.
    
    The default OpenSSH Server configuration (Windows 10+) has `sshd`
    running as the built-in NT user NETWORK_SERVICE, which means it runs in
    session 0 (the services session). This is most common scenario, other
    than using WSL from a 'real', interactive Windows session that we're
    likely to face.

--- src/shared/Core/BrowserUtils.cs
+++ src/shared/Core/BrowserUtils.cs
@@ -15,6 +15,43 @@ namespace GitCredentialManager
 
         private static bool GetWebBrowserAvailable(IEnvironment env, IFileSystem fs, ISessionManager sm)
         {
+            // If this is a Windows Subsystem for Linux distribution we may
+            // be able to launch the web browser of the host Windows OS.
+            if (PlatformUtils.IsLinux() && WslUtils.IsWslDistribution(env, fs, out _))
+            {
+                // We need a shell execute handler to be able to launch to browser
+                if (!TryGetLinuxShellExecuteHandler(env, out _))
+                {
+                    return false;
+                }
+
+                //
+                // If we are in Windows logon session 0 then the user can never interact,
+                // even in the WinSta0 window station. This is typical when SSH-ing into a
+                // Windows 10+ machine using the default OpenSSH Server configuration,
+                // which runs in the 'services' session 0.
+                //
+                // If we're in any other session, and in the WinSta0 window station then
+                // the user can possibly interact. However, since it's hard to determine
+                // the window station from PowerShell cmdlets (we'd need to write P/Invoke
+                // code and that's just messy and too many levels of indirection quite
+                // frankly!) we just assume any non session 0 is interactive.
+                //
+                // This assumption doesn't hold true if the user has changed the user that
+                // the OpenSSH Server service runs as (not a built-in NT service) *AND*
+                // they've SSH-ed into the Windows host (and then started a WSL shell).
+                // This feels like a very small subset of users...
+                //
+                if (WslUtils.GetWindowsSessionId(fs) == 0)
+                {
+                    return false;
+                }
+
+                // If we are not in session 0, or we cannot get the Windows session ID,
+                // assume that we *CAN* launch the browser so that users are never blocked.
+                return true;
+            }
+
             // We require an interactive desktop session to be able to launch a browser
             return sm.IsDesktopSession;
         }
@@ -39,7 +76,7 @@ namespace GitCredentialManager
 
             string url = uri.ToString();
 
-            ProcessStartInfo psi = null;
+            ProcessStartInfo psi;
             if (PlatformUtils.IsLinux())
             {
                 //
@@ -55,28 +92,16 @@ namespace GitCredentialManager
                 // We try and use the same 'shell execute' utilities as the Framework does,
                 // searching for them in the same order until we find one.
                 //
-                // One additional 'shell execute' utility we also attempt to use is `wslview`
-                // that is commonly found on WSL (Windows Subsystem for Linux) distributions that
-                // opens the browser on the Windows host.
-                foreach (string shellExec in new[] {"xdg-open", "gnome-open", "kfmclient", "wslview"})
-                {
-                    if (environment.TryLocateExecutable(shellExec, out string shellExecPath))
-                    {
-                        psi = new ProcessStartInfo(shellExecPath, url)
-                        {
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true
-                        };
-
-                        // We found a way to open the URI; stop searching!
-                        break;
-                    }
-                }
-
-                if (psi is null)
+                if (!TryGetLinuxShellExecuteHandler(environment, out string shellExecPath))
                 {
                     throw new Exception("Failed to locate a utility to launch the default web browser.");
                 }
+
+                psi = new ProcessStartInfo(shellExecPath, url)
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                };
             }
             else
             {
@@ -86,6 +111,24 @@ namespace GitCredentialManager
             }
 
             Process.Start(psi);
+        }
+
+        private static bool TryGetLinuxShellExecuteHandler(IEnvironment env, out string shellExecPath)
+        {
+            // One additional 'shell execute' utility we also attempt to use over the Framework
+            // is `wslview` that is commonly found on WSL (Windows Subsystem for Linux) distributions
+            // that opens the browser on the Windows host.
+            string[] shellHandlers = { "xdg-open", "gnome-open", "kfmclient", WslUtils.WslViewShellHandlerName };
+            foreach (string shellExec in shellHandlers)
+            {
+                if (env.TryLocateExecutable(shellExec, out shellExecPath))
+                {
+                    return true;
+                }
+            }
+
+            shellExecPath = null;
+            return false;
         }
     }
 }
